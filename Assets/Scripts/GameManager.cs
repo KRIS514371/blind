db7f415effd95fe052d6de1fb4d8365b550fa2f0
KRIS514371 <1960447942@qq.com>
2025-03-07 20:16:13 +0800

    1234

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(Instance);
+        }
+    }
+
+    public StoryManager storyManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Additive);   
+    }
+
+    public void UnloadScene(string name)
+    {
+        SceneManager.UnloadSceneAsync(name);
+    }
+}
---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour
+{
+
+
+
+
+
+
+    public void LoadStory()
+    { 
+    
+    }
+
+}
