c3e5c6e1d703eb200aba33c2f31aa768a100863c
wshjle <wshjlgg@hotmail.com>
2025-09-07 12:08:54 +0800

    0907-

--- Assets/Scripts/PaperSkill.cs
+++ Assets/Scripts/PaperSkill.cs
@@ -1,8 +1,11 @@
 using System.Collections;
+using Oculus.Interaction;
 using UnityEngine;
 
 public class PaperSkill : MonoBehaviour
 {
+    public GrabInteractable grabPaper;
+    public bool enableSkill = false;
     public Transform handRef;
     public Transform Paper;
     public ParticleSystem particle;
@@ -10,16 +13,33 @@ public class PaperSkill : MonoBehaviour
     void Start()
     {
         Paper.gameObject.SetActive(false);
+        particle.Stop();
+    }
+
+    void CheckPick(InteractableStateChangeArgs  args)
+    {
+        // if (grabPaperInteractor.State == InteractorState.Select)
+        // {
+        //     Debug.Log("成功拿取" + grabPaperInteractor.State);
+        //     enableSkill = true;
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = handRef.transform.position;
+        transform.rotation = handRef.transform.rotation;
+    }
+
+    public void EnableSkill()
+    {
+        enableSkill = true;
     }
 
     public void Trigger()
     {
+        if(!enableSkill) return;
         Debug.Log("Trigger");
         Paper.gameObject.SetActive(true);
         particle.Play();
@@ -28,6 +48,7 @@ public class PaperSkill : MonoBehaviour
     
     public void UnTrigger()
     {
+        if(!enableSkill) return;
         Debug.Log("Trigger");
         Paper.gameObject.SetActive(false);
         particle.Stop();
