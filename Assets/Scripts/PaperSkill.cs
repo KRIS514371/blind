e4beb86510a5ac7482f2dc5e5c05fef8123eff0e
wshjle <wshjlgg@hotmail.com>
2025-09-21 21:50:19 +0800

    0921

--- Assets/Scripts/PaperSkill.cs
+++ Assets/Scripts/PaperSkill.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using Oculus.Interaction;
+using Oculus.Interaction.GrabAPI;
+using Oculus.Interaction.HandGrab;
 using UnityEngine;
 
 public class PaperSkill : MonoBehaviour
 {
-    public GrabInteractable grabPaper;
+    public GameObject grabPaperItem;
+    public HandGrabInteractable grabPaper;
     public bool enableSkill = false;
     public Transform handRef;
     public Transform Paper;
@@ -14,6 +17,23 @@ public class PaperSkill : MonoBehaviour
     {
         Paper.gameObject.SetActive(false);
         particle.Stop();
+        grabPaper.WhenStateChanged += StateChanged;
+        
+    }
+
+    void StateChanged(InteractableStateChangeArgs arg)
+    {
+        Debug.Log("grabPaper NewState:" +arg.NewState);
+        if (arg.NewState == InteractableState.Select)
+        {
+            enableSkill = true;
+        }
+
+        if ( arg.PreviousState == InteractableState.Select &&  arg.NewState == InteractableState.Normal)
+        {
+            //Todo:触发消散
+            grabPaperItem.SetActive(false);
+        }
     }
 
     void CheckPick(InteractableStateChangeArgs  args)
